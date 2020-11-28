d147bf0b4a1d3b6c6455af518e159fbcfef61596
규동 김 <batebono@gmail.com>
2020-11-28 20:55:58 +0900

    Lecture9

---
+++ Class7/Lecture9/Lecture9.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	int temp;
+	temp = true ? 1024 : 7;	// ternary
+	printf("%d\n", temp);
+
+	temp = false ? 1024 : 7;
+	printf("%d\n", temp);
+
+
+	//int a = 1, b = 2;
+	//int max = (a > b) ? a : b;
+
+
+	//int number;
+	//scanf("%d", &number);
+
+	//const bool is_even = (number % 2 == 0) ? true : false;
+	// 
+	//is_even ? printf("Even") : printf("Odd");
+
+}
