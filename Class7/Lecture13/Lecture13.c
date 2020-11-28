965eee7b28978f0917510b25e2972b6dee346cf3
규동 김 <batebono@gmail.com>
2020-11-28 21:29:55 +0900

    Lecture13

---
+++ Class7/Lecture13/Lecture13.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int size = 15, cost;
+
+	if (size < 10)
+		goto a;
+	goto b;
+
+a:  cost = 50 * size;
+b:  cost = 100 * size;
+
+	if (size < 10)
+		cost = 50 * size;
+	else
+		cost = 100 * size;
+
+read: c = getchar();
+	putchar(c);
+	if (c == '.') goto quit;
+	goto read;
+quit:
+
+	while (1) {
+		c = getchar();
+		putchar(c);
+		if (c == '.') break;
+	}
+
+
+}
