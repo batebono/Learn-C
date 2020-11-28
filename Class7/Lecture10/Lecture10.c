c8e237109167d437ec7de570c0169b2858aab99f
규동 김 <batebono@gmail.com>
2020-11-28 21:11:24 +0900

    Lecture10

---
+++ Class7/Lecture10/Lecture10.c
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	//for (int i = 0; i < 10; ++i) {
+	//	if (i == 5)
+	//		continue;
+
+	//	printf("%d ", i);
+
+	//	if (i != 5)
+	//		printf("%d ", i);
+	//}
+
+	/* break */
+	//for (int i = 0; i < 10; ++i) {
+	//	if (i == 5)
+	//		break;
+
+	//	printf("%d ", i);
+	//}
+
+	//int count = 0;
+	//while (count < 5)
+	//{
+	//	int c = getchar();
+	//	if (c == 'a')
+	//		continue;
+	//	putchar(c);
+	//	count++;
+	//};
+
+	/* continue as a placeholder */
+	//while (getchar() != '\n')
+	//	continue;
+
+	//while (1)
+	//{
+	//	char c = getchar();
+
+	//	if (c == '.')
+	//		break;
+
+	//	putchar(c);
+	//}
+}
