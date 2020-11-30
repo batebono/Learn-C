c9eeba54910fc36c6ea413b5df95ab76422cedd9
규동 김 <batebono@gmail.com>
2020-11-30 19:12:08 +0900

    Lecture5

---
+++ Class8/Lecture5/Lecture5.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void display(char cr, int lines, int width);
+
+int main()
+{
+	char c; 
+	int rows, cols;
+
+	//while (1)
+	//{
+	//	scanf("%c %d %d", &c, &rows, &cols);
+	//	while (getchar() != '\n');
+	//	display(c, rows, cols);
+	//	if (c == '\n')
+	//		break;
+	//}
+
+	while ((c = getchar()) != '\n')
+	{
+		scanf("%d %d", &rows, &cols);
+		while (getchar() != '\n') continue;
+		display(c, rows, cols);
+		printf("Input another charcter and two intergers:\n");
+		printf("Press Enter to quit.\n");
+	}
+
+}
+
+
+void display(char cr, int lines, int width)
+{
+	int i, j;
+	for (i = 0; i < lines; ++i)
+	{
+		for (j = 0; j < width; j++)
+			printf("%c", cr);
+		printf("\n");
+	}
+}
