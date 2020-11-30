82a41449f0701a7844fa8beec3dbb8f02b17ad40
규동 김 <batebono@gmail.com>
2020-11-30 17:15:29 +0900

    Lecture

--- Class8/Lecture1/Lecture1.c
+++ Class8/Lecture1/Lecture1.c
@@ -1,8 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <conio.h>//Windows, _getch(), _getche()
+
 
 int main()
 {
+	char c;
 
+	//while ((c = getchar()) != '.')
+	//	putchar(c);
+
+	while ((c = _getche()) != '.')//echo
+		putchar(c);
 
 }
