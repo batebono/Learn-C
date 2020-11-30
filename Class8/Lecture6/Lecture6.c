245f689e99c7046acc7c2e9ea0f689c411053b82
규동 김 <batebono@gmail.com>
2020-11-30 19:58:58 +0900

    Lecture8

---
+++ Class8/Lecture6/Lecture6.c
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+long get_long(void);
+
+int main()
+{
+	long number;
+
+	printf("Please input an integer between 1 and 100.\n");
+	while (1)
+	{
+		number = get_long();
+
+		if (number > 1 && number < 100) {
+			printf("Ok. Thank you.\n");
+			break;
+		}
+		else
+			printf("Wrong input.\n");
+	}
+
+	printf("Your input %d is between 1 and 100. Thank you.\n", number);
+	return 0;
+}
+
+long get_long(void)
+{
+	printf("Please input an interger and press enter.\n");
+
+	long input;
+	char c;
+
+	while (scanf("%ld", &input) != 1)
+	{
+		printf("Your input - ");
+
+		while ((c = getchar()) != '\n')
+			putchar(c);
+
+		printf(" - is not an interger. Please try again.\n");
+	}
+
+	printf("Your input %ld is an integer. Thank you.\n", input);
+
+	return input;
+}
